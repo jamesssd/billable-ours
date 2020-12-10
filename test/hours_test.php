209ae073f45ef5854f00be453a29a1b6073a6067
James Dizon <47347463+jamesssd@users.noreply.github.com>
2020-12-10 09:41:16 -0800

    Create hours_test.php

---
+++ test/hours_test.php
@@ -0,0 +1,34 @@
+<?php
+
+//login_form_test.php
+include_once 'includes/settings.php';
+require_once 'simpletest/autorun.php';
+require_once 'simpletest/web_tester.php';
+
+class HoursForm extends WebTestCase {
+
+	  function testCorrectData() {
+		$this->get(VIRTUAL_PATH . '/login.php');
+		$this->assertResponse(200);
+
+    $this->setField("hours", "2");
+		$this->setField("rate", "50");
+		$this->clickSubmit("Show Pay");
+
+		$this->assertResponse(200);
+		$this->assertText("You input 2 hours at a rate of $2 and your pay is $100");
+	}
+	
+ 	 function testFailedPassword() {
+		$this->get(VIRTUAL_PATH . '/login.php');
+		$this->assertResponse(200);
+
+		$this->setField("name", "John");
+		$this->setField("password", "XYZXYZ");//incorrect password
+		$this->clickSubmit("Login");
+
+		$this->assertResponse(200);
+		$this->assertText("Login and/or password is incorrect");
+	}
+
+}
